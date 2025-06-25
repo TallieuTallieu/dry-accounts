0eab3dc4b35a9ea1235cfe9e1a2e2a0318cef381
Korneel Eeckhout <korneel@pienter.tech>
2025-06-25 12:31:09 +0200

    Add PHPDoc method annotations to Auth facade
    Improves IDE autocomplete and type inference for facade methods.

--- src/Facade/Auth.php
+++ src/Facade/Auth.php
@@ -4,7 +4,16 @@ namespace Tnt\Account\Facade;
 
 use Oak\Facade;
 use Tnt\Account\Contracts\AuthenticationInterface;
+use Tnt\Account\Contracts\User\UserInterface;
 
+/**
+ * @method static UserInterface|null register(string $authIdentifier, string $password)
+ * @method static bool authenticate(string $authIdentifier, string $password)
+ * @method static mixed logout()
+ * @method static bool isAuthenticated()
+ * @method static UserInterface|null getUser()
+ * @method static UserInterface|null getActivatedUser(string $authIdentifier)
+ */
 class Auth extends Facade
 {
     protected static function getContract(): string
